2d65d98c10d47aea339147e8d570c447af9b80d6
Laurent Mignot <laurent@mignot.eu>
2017-07-28 09:43:39 +0100

    Add getter for AppLocationProvider to App

--- Android/App/app/src/main/java/eu/mignot/pathogentracker/App.kt
+++ Android/App/app/src/main/java/eu/mignot/pathogentracker/App.kt
@@ -1,7 +1,23 @@
 package eu.mignot.pathogentracker
 
 import android.app.Application
+import com.patloew.rxlocation.RxLocation
+import eu.mignot.pathogentracker.data.AppLocationProvider
 
 class App : Application() {
+
+  val locationProvider by lazy {
+    AppLocationProvider(RxLocation(this))
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    instance = this
+  }
+
+  companion object {
+    lateinit var instance: App private set
+    fun getLocationProvider() = instance.locationProvider
+  }
 
 }
