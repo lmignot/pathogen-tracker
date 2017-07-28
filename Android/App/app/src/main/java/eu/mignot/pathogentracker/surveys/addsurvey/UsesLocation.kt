6edd448b25dc65dad387b913eeb139b6b98bee72
Laurent Mignot <laurent@mignot.eu>
2017-07-28 09:38:45 +0100

    UsesLocation
    - interface to handle location permissions

---
+++ Android/App/app/src/main/java/eu/mignot/pathogentracker/surveys/addsurvey/UsesLocation.kt
@@ -0,0 +1,29 @@
+package eu.mignot.pathogentracker.surveys.addsurvey
+
+import android.Manifest
+import android.app.Activity
+import me.zhanghai.android.effortlesspermissions.AfterPermissionDenied
+import me.zhanghai.android.effortlesspermissions.EffortlessPermissions
+import pub.devrel.easypermissions.AfterPermissionGranted
+
+interface UsesLocation {
+  companion object {
+    const val REQUEST_CODE = 1
+    val PERMISSION = (Manifest.permission.ACCESS_FINE_LOCATION)
+  }
+
+  fun askForLocationPermission(activity: Activity, rationale: String) {
+    EffortlessPermissions.requestPermissions(
+      activity,
+      rationale,
+      REQUEST_CODE,
+      PERMISSION
+    )
+  }
+
+  @AfterPermissionGranted(REQUEST_CODE)
+  fun requestLocationPermission() {}
+
+  @AfterPermissionDenied(REQUEST_CODE)
+  fun onLocationPermissionDenied() {}
+}
