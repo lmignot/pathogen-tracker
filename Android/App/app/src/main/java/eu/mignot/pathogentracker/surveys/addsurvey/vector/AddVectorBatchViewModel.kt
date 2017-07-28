053f535cb0b89b310195827d4cb2163c54d52485
Laurent Mignot <laurent@mignot.eu>
2017-07-28 09:42:59 +0100

    AddVectorBatchViewModel:
    - getlocation
    
    - save stub

--- Android/App/app/src/main/java/eu/mignot/pathogentracker/surveys/addsurvey/vector/AddVectorBatchViewModel.kt
+++ Android/App/app/src/main/java/eu/mignot/pathogentracker/surveys/addsurvey/vector/AddVectorBatchViewModel.kt
@@ -1,13 +1,17 @@
 package eu.mignot.pathogentracker.surveys.addsurvey.vector
 
+import io.reactivex.Observable
 import io.reactivex.Single
 import java.util.*
 import eu.mignot.pathogentracker.data.LocationProvider
 import eu.mignot.pathogentracker.data.models.Location
+import eu.mignot.pathogentracker.surveys.data.models.survey.VectorBatch
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
 class AddVectorBatchViewModel(
   val locationProvider: LocationProvider
-) {
+): AnkoLogger {
 
   private val currentDate by lazy { Calendar.getInstance() }
 
@@ -17,10 +21,11 @@ class AddVectorBatchViewModel(
     get() = currentDate
     set(d) = setCurrentDate(d)
 
-  fun getLocation(): Single<Location> = locationProvider.getLocation()
+  fun getLocation(): Observable<Location> = locationProvider.getLocation()
 
-  fun save(): Single<Boolean> {
-    TODO("NI")
+  fun save(model: VectorBatch): Single<Boolean> {
+    info {model.toString()}
+    return Single.just(true)
   }
 
   private fun setCurrentDate(d: Calendar): Unit {
